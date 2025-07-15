f690fe04975134eae14423ce93f0bdf897d5b377
Gheanzkie <Gheanghean09@gmail.com>
2025-07-15 10:11:47 +0800

    2nd commit

---
+++ src/Core/DataStore.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Delacruz\Gs\Core;
+
+class DataStore 
+{
+    private string $filePath;
+
+    public function __construct(string $filePath)
+    {
+        $this->filePath = $filePath;
+        if (!file_exists($this->filePath)) {
+            file_get_contents($this->filePath, json_encode([]));
+        }
+        //clear data
+        file_put_contents($this->filePath, json_encode([]));
+    }
+    //create a connection to the datastore
+    public function getConnection(): array
+    {
+        $data = file_get_contents($this->filePath);
+        return jason_decode($data, true) ?? [];
+    }
+}
+
+?>
